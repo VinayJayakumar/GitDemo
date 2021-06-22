1bc2d18774a55cc84af2bf787935fc6c815717ea
AnnuGupta2707 <annugupta2707@gmail.com>
2021-06-22 19:08:31 +0530

    Final Commit

---
+++ src/test/java/testNG_Link/Test_ClassDemo3.java
@@ -0,0 +1,26 @@
+package testNG_Link;
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import testNG_basics.AbstractChromeWebDriver;
+
+
+public class Test_ClassDemo3 extends AbstractChromeWebDriver {
+  @Test
+  public void login() {
+	  
+	  Webdriver.get("http://localhost:8080/pages/index.html");
+	  sleep(5);
+	  List<WebElement> classElements = Webdriver.findElements(By.className("huge"));
+	  sleep(2);
+	  for ( WebElement classElement:classElements ) {
+		  System.out.println(classElement.getText());
+	  }
+  }
+}
